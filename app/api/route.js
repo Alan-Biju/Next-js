b4f4691d7cbeca81b4efd19dbd04b9b9ac02852d
Alan Biju <alan.biju@fisdom.com>
2024-08-13 16:43:35 +0530

    test

--- app/api/route.js
+++ app/api/route.js
@@ -1,10 +1,16 @@
 import { NextResponse } from 'next/server';
 import path from 'path';
+import fs from 'fs';
 
 
 export async function GET() {
   const dirPath = path.join(process.cwd())
-  return NextResponse.json({ msg: 'Hello from server', dirPath });
+  const files = await fs.promises.readdir(dirPath, { withFileTypes: true });
+
+  let folderNames = files
+    .filter(file => file.isDirectory())
+    .map(file => file.name);
+  return NextResponse.json({ msg: 'Hello from server', folderNames });
 }
 
 export async function POST(request) {
