b7575d77d105833b48071cf00e8a1503a5121f3e
Alan Biju <alan.biju@fisdom.com>
2024-08-13 14:14:42 +0530

    test

---
+++ app/alan/page.jsx
@@ -0,0 +1,7 @@
+const Page = ({}) => {
+  return <div>
+    hello world this create from default template
+  </div>
+}
+
+export default Page
--- app/api/route.js
+++ app/api/route.js
@@ -3,3 +3,8 @@ import { NextResponse } from 'next/server'
 export async function GET(request) {
   return NextResponse.json({ msg: 'Hello from server' })
 }
+
+export async function POST(request) {
+    const data =  await request.json()
+    return NextResponse.json({ data: data })
+  }
