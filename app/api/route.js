e708d3999a2dcfca8a372580d80eab981a5b36f0
Alan Biju <alan.biju@fisdom.com>
2024-08-13 16:11:46 +0530

    test

--- app/api/route.js
+++ app/api/route.js
@@ -1,10 +1,10 @@
-import { NextResponse } from 'next/server'
- 
-export async function GET(request) {
-  return NextResponse.json({ msg: 'Hello from server' })
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  return NextResponse.json({ msg: 'Hello from server' });
 }
 
 export async function POST(request) {
-    const data =  await request.json()
-    return NextResponse.json({ data: data })
-  }
+  const data = await request.json();
+  return NextResponse.json({ data: data });
+}
